03d951163acf80c4b3e270ef96c2b3baee38aa45
quentin roux <quentin.roux@hotmail.fr>
2020-06-24 12:04:30 +0200

    deploy worflow test

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,7 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT secret must be defined');
   }
+
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI must be defined');
   }
